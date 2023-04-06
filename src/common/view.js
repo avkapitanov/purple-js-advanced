e4802cd1a084f67c0126b87d17c91aa693fed93d
Artem Kapitanov <avkapitanov@yandex.ru>
2023-04-06 22:05:15 +0300

    feat: add first view and route

---
+++ src/common/view.js
@@ -0,0 +1,13 @@
+export class AbstractView {
+  constructor() {
+    this.app = document.getElementById('root');
+  }
+
+  setTitle(title) {
+    document.title = title;
+  }
+
+  render() {}
+
+  destroy() {}
+}
