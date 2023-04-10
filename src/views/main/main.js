d18f848f3ae27751d2e608929a6430d70b3562f3
Artem Kapitanov <avkapitanov@yandex.ru>
2023-04-10 21:51:36 +0300

    feat: add search functional

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -15,12 +15,27 @@ export class MainView extends AbstractView {
     super();
     this.appState = appState;
     this.appState = onChange(this.appState, this.onUpdateAppState.bind(this))
+    this.state = onChange(this.state, this.onUpdateState.bind(this))
     this.setTitle('Поиск книг')
   }
 
   onUpdateAppState(path) {
     if (path === 'favorites') {
       this.render();
+    }
+  }
+
+  async loadBookList(q, offset) {
+    const res = await fetch(`https://openlibrary.org/search.json=?q=${q}&offset=${offset}`);
+    return res.json();
+  }
+
+  async onUpdateState(path) {
+    if (path === 'searchQuery') {
+      this.state.loading = true;
+      const data = await this.loadBookList(this.state.searchQuery, this.state.offset);
+      this.state.loading = false;
+      this.state.list = data.docs;
     }
   }
 
