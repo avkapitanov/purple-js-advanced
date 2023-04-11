1e3d7b384abec2e08e4ef7c58c21c42c1e633418
Artem Kapitanov <avkapitanov@yandex.ru>
2023-04-11 21:42:29 +0300

    feat: add card component

---
+++ src/components/card-list/card-list.js
@@ -0,0 +1,28 @@
+import { DivComponent } from '../../common/div-component.js';
+import './card-list.css';
+import { Card } from '../card/card.js';
+
+export class CardList extends DivComponent {
+  constructor(appState, parentState) {
+    super();
+    this.appState = appState;
+    this.parentState = parentState;
+  }
+
+  render() {
+    if (this.parentState.loading) {
+      this.el.innerHTML = `<div class="card_list__loader">Загрузка...</div>`;
+      return this.el;
+    }
+
+    this.el.classList.add('card-list');
+    this.el.innerHTML = `
+      <h1>Найдено книг - ${this.parentState.numFound}</h1>
+     `;
+    for (const card of this.parentState.list) {
+      this.el.append(new Card(this.appState, card).render());
+    }
+
+    return this.el;
+  }
+}
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,12 +2,14 @@ import { AbstractView } from '../../common/view.js';
 import onChange from 'on-change';
 import { Header } from '../../components/header/header.js';
 import { Search } from '../../components/search/search.js';
+import { CardList } from '../../components/card-list/card-list.js';
 
 export class MainView extends AbstractView {
   state = {
     list: [],
     loading: false,
     searchQuery: undefined,
+    numFound: 0,
     offset: 0
   };
 
@@ -26,7 +28,7 @@ export class MainView extends AbstractView {
   }
 
   async loadBookList(q, offset) {
-    const res = await fetch(`https://openlibrary.org/search.json=?q=${q}&offset=${offset}`);
+    const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
     return res.json();
   }
 
@@ -36,12 +38,17 @@ export class MainView extends AbstractView {
       const data = await this.loadBookList(this.state.searchQuery, this.state.offset);
       this.state.loading = false;
       this.state.list = data.docs;
+      this.state.numFound = data.numFound;
+    }
+    if (path === 'list' || path === 'loading' || path === 'numFound') {
+      this.render();
     }
   }
 
   render() {
     const main = document.createElement('div');
     main.append(new Search(this.state).render());
+    main.append(new CardList(this.appState, this.state).render());
     this.app.innerHTML = '';
     this.app.append(main);
     this.renderHeader();
